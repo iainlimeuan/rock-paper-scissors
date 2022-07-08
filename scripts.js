5cb48f29907d36201355f60b38a8e6ce958c2750
kurdock <iainlim853@gmail.com>
2022-07-09 03:03:03 +0800

    Added playGame() and game() functions
    playGame is currently mostly a bunch of crude nested ifs, which
    I suspect can be changed to some sort of switch or case code.

--- scripts.js
+++ scripts.js
@@ -4,4 +4,45 @@ function computerPlay() {
     return choices[randomNumber];
 }
 
-console.log(computerPlay())
+function playGame(computerPlay, playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (computerPlay == "rock") {
+        if (playerSelection == "rock") {
+            return "It's a tie!";
+        }
+        else if (playerSelection == "paper") {
+            return "You win! Paper beats rock.";
+        }
+        else return "You lose! Rock beats scissors.";
+    }
+
+    else if (computerPlay == "paper") {
+        if (playerSelection == "paper") {
+            return "It's a tie!";
+        }
+        else if (playerSelection == "rock") {
+            return "You lose! Paper beats rock.";
+        }
+        else return "You win! Scissors beats paper."
+    }
+
+    else if (computerPlay == "scissors") {
+        if (playerSelection == "scissors") {
+            return "It's a tie!";
+        }
+        else if (playerSelection == "paper") {
+            return "You lose! Scissors beats paper.";
+        }
+        else return "You win! Rock beats scissors."
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Choose rock, paper or scissors!");
+        console.log(playGame(computerPlay(),playerSelection));
+    }
+}
+
+game();
+
